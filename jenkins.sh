e468688bc1ffa6fdf0e45daa561564cf8c69d547
Tulshiram Kadam <tulshiramcloud@gmail.com>
2024-06-05 00:58:26 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -38,3 +38,5 @@ curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin
 kubectl version --short --client
+
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
